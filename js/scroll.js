652ce9597aac1446f9e4dc2cffba52f050a8c918
oilPersonage <oilfreelance@ya.ru>
2024-09-30 21:44:10 +0700

    change locomotive to v4

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,4 @@
 import LocomotiveScroll from "locomotive-scroll";
-import throttle from "lodash.throttle";
 
 Number.prototype.clamp = function (min, max) {
   return Math.min(Math.max(this, min), max);
@@ -16,60 +15,68 @@ function pauseEvent(e) {
 const heightThumb = 126;
 const offsetTopScrollbar = 24;
 
-const scrollbarThumb = document.querySelector(".scrollbar-thumb");
-const scrollbar = document.querySelector(".scrollbar");
-const scrollbarHeight = scrollbar.offsetHeight - heightThumb;
+// const scrollbarThumb = document.querySelector(".scrollbar-thumb");
+// const scrollbar = document.querySelector(".scrollbar");
+// const scrollbarHeight = scrollbar.offsetHeight - heightThumb;
 let position = 0;
 let drag = false;
 const scrollbarThumbCenter = heightThumb / 2 - offsetTopScrollbar;
 
-function onScroll({ scroll, limit, velocity, direction, progress }) {
-  scrollbarThumb.style.transform = `translateY(${progress * scrollbarHeight}px)`;
-  scrollbarThumb.style.opacity = Math.ceil(Math.abs(velocity)).toString();
-  // console.log( scroll)
-}
+// function onScroll({ scroll, limit, velocity, direction, progress }) {
+//   scrollbarThumb.style.transform = `translateY(${progress * scrollbarHeight}px)`;
+//   scrollbarThumb.style.opacity = Math.ceil(Math.abs(velocity)).toString();
+//   // console.log( scroll)
+// }
 
 const locomotiveScroll = new LocomotiveScroll({
-  lenisOptions: {
-    wrapper: document.querySelector("#main"),
-    content: document.querySelector(".scroll-container"),
-    lerp: 0.1,
-    duration: 1.2,
-    orientation: "vertical",
-    gestureOrientation: "vertical",
-    smoothWheel: true,
-    smoothTouch: false,
-    wheelMultiplier: 1,
-    touchMultiplier: 1,
-    normalizeWheel: true,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  // el: document.querySelector("#main"),
+  el: document.querySelector(".scroll-container"),
+  lerp: 0.1,
+  duration: 1.2,
+  orientation: "vertical",
+  gestureOrientation: "vertical",
+  scrollbarContainer: document.querySelector(".scrollbar"),
+  scrollbarClass: "scrollbar",
+  smooth: true,
+  smartphone: {
+    smooth: true,
   },
-  scrollCallback: onScroll,
+  smoothWheel: true,
+  smoothTouch: false,
+  wheelMultiplier: 1,
+  touchMultiplier: 1,
+  normalizeWheel: true,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  // scrollCallback: onScroll,
 });
+//
+// scrollbarThumb.ondragstart = function (e) {
+//   return false;
+// };
+//
+// scrollbarThumb.addEventListener("mousedown", (e) => {
+//   pauseEvent(e);
+//   drag = true;
+// });
+// document.addEventListener("mouseup", (e) => {
+//   drag = false;
+// });
+// document.addEventListener("mouseleave", (e) => {
+//   drag = false;
+// });
+// document.addEventListener(
+//   "mousemove",
+//   throttle((e) => {
+//     if (!drag) return;
+//     const progress = (
+//       (e.clientY - scrollbarThumbCenter) /
+//       window.innerHeight
+//     ).clamp(0, 1);
+//     position = progress * document.body.scrollHeight;
+//     locomotiveScroll.scrollTo(position);
+//   }, 100),
+// );
 
-scrollbarThumb.ondragstart = function (e) {
-  return false;
-};
-
-scrollbarThumb.addEventListener("mousedown", (e) => {
-  pauseEvent(e);
-  drag = true;
-});
-document.addEventListener("mouseup", (e) => {
-  drag = false;
-});
-document.addEventListener("mouseleave", (e) => {
-  drag = false;
-});
-document.addEventListener(
-  "mousemove",
-  throttle((e) => {
-    if (!drag) return;
-    const progress = (
-      (e.clientY - scrollbarThumbCenter) /
-      window.innerHeight
-    ).clamp(0, 1);
-    position = progress * document.body.scrollHeight;
-    locomotiveScroll.scrollTo(position);
-  }, 100),
-);
+function animateH2(e) {
+  console.log(e);
+}
