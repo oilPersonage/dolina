8576ad8285487c19f5f8d5e719d848d0d5df8ec2
oilPersonage <oilfreelance@ya.ru>
2024-10-01 10:32:20 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "dolina",
+  base: "/dolina/",
   build: {
     sourcemap: true,
     // Reduce bloat from legacy polyfills.
